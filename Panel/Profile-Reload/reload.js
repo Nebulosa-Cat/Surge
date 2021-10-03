f14d18d031cb04f9156dd93a3b5d9bb8d72ed0e9
雲豹ΦωΦ <armfire1911@protonmail.com>
2021-10-03 14:15:59 +0800

    update

--- Panel/Profile-Reload/reload.js
+++ Panel/Profile-Reload/reload.js
@@ -1,9 +1,9 @@
 $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $notification.post("配置重載","成功","")
     $done({
-        icon: "arrow.triangle.2.circlepath.doc.on.clipboard",
-        title: "Profiles Reload",
+        title: "Profile Reload",
         content: "配置檔案重載成功",
-        "icon-color": "#8B81C3"
+        icon: "arrow.triangle.2.circlepath.doc.on.clipboard",
+        "icon-color": "#8B81C3",
      })
     });
