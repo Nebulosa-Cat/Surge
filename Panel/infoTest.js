58106d7f74206c94d8936c4f5f2f3437e3b97681
雲豹 ΦωΦ <armfire1911@protonmail.com>
2022-01-25 11:07:28 +0800

    Update infoTest.js

--- Panel/infoTest.js
+++ Panel/infoTest.js
@@ -58,8 +58,7 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
             });
         } else {
             const info = JSON.parse(data);
-            if ($info[province] === undefined) {
-                const info = JSON.parse(data);
+            if (info.province === undefined) {
                 $done({
                     title: wifi.ssid ? wifi.ssid : cellularInfo,
                     content:
@@ -71,7 +70,6 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
                     'icon-color': wifi.ssid ? '#007AFF' : '#F9BF45',
                 });
             } else {
-                const info = JSON.parse(data);
                 $done({
                     title: wifi.ssid ? wifi.ssid : cellularInfo,
                     content:
