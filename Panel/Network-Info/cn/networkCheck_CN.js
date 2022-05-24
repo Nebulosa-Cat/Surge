e8864ab57c14fa0cc7d0b87d0e406f1db5f77195
Nebulosa Cat <work@nebulosa-cat.com>
2022-05-24 17:35:18 +0800

    文本調整

--- Panel/Network-Info/cn/networkCheck_CN.js
+++ Panel/Network-Info/cn/networkCheck_CN.js
@@ -56,7 +56,7 @@ if (!v4.primaryAddress && !v6.primaryAddress) {
     $done({
       title: wifi.ssid ? wifi.ssid : cellularInfo,
       content:
-        `[IP地址]\n` +
+        `[IP 地址]\n` +
         (v4.primaryAddress ? `v4 @ ${v4.primaryAddress} \n` : '') +
         (v6.primaryAddress ? `v6 @ ${v6.primaryAddress}\n` : '') +
         (v4.primaryRouter && wifi.ssid ? `Router v4 @ ${v4.primaryRouter}\n` : '') +
