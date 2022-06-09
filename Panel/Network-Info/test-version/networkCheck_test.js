a330454be03f635d5f326327129f373bc57dac47
Nebulosa Cat <work@nebulosa-cat.com>
2022-06-09 19:49:37 +0800

    test eval

--- Panel/Network-Info/test-version/networkCheck_test.js
+++ Panel/Network-Info/test-version/networkCheck_test.js
@@ -25,7 +25,7 @@ const radioGeneration = {
 
 const carrierNames = loadCarrierNames();
 
-setTimeout(getNetworkInfo, 0);
+setTimeout('getNetworkInfo()', 50000);
 
 function getNetworkInfo() {
   if (!v4.primaryAddress && !v6.primaryAddress) {
