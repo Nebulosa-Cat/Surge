3b53b690b89902f2293f70e1abb988a73bbcda53
Nebulosa Cat <work@nebulosa-cat.com>
2024-01-27 19:09:26 +0800

    Update reload.js

--- Panel/Profile-Reload/reload.js
+++ Panel/Profile-Reload/reload.js
@@ -3,5 +3,7 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $done({
         title: "Profile Reload",
         content: "配置檔案重載成功",
+        icon: "{{{icon}}}",
+        "icon-color": "{{{iconColor}}}",
      })
     });
