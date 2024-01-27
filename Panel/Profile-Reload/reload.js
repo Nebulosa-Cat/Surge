ddace856d374e740695e709d5e09fc297c332b42
Nebulosa Cat <work@nebulosa-cat.com>
2024-01-27 19:06:02 +0800

    錯誤修正
    目前似乎僅能適用於js檔內無指定狀態變化後圖標與顏色的模組

--- Panel/Profile-Reload/reload.js
+++ Panel/Profile-Reload/reload.js
@@ -3,7 +3,5 @@ $httpAPI("POST", "/v1/profiles/reload", {}, data => {
     $done({
         title: "Profile Reload",
         content: "配置檔案重載成功",
-        icon: "arrow.triangle.2.circlepath.doc.on.clipboard",
-        "icon-color": "#8B81C3",
      })
     });
