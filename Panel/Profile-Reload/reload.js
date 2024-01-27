336d0c267410d97c1c49cbc8e2dd45ec432108e2
Nebulosa Cat <work@nebulosa-cat.com>
2024-01-27 19:52:56 +0800

    修正提前執行

--- Panel/Profile-Reload/reload.js
+++ Panel/Profile-Reload/reload.js
@@ -1,16 +1,17 @@
 !(async () => {
-    let panel = { title: "Profile Reload"}
+    let panel = { title: "Profile Reload" }
 
     if (typeof $argument != "undefined") {
         let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
         if (arg.title) panel.title = arg.title;
         if (arg.icon) panel.icon = arg.icon;
         if (arg.color) panel["icon-color"] = arg.color;
-        if (arg.server == "false") showServer = false;
     }
-    if ($trigger == "button") await httpAPI("/v1/profiles/reload");
-    panel.content = `配置檔案重載成功`;
-    $done(panel);
+    if ($trigger == "button") {
+        await httpAPI("/v1/profiles/reload");
+        panel.content = `配置檔案重載成功`;
+        $done(panel);
+    }
 })();
 
 function httpAPI(path = "", method = "POST", body = null) {
