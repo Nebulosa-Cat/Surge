38481384e79f71026e10e7721ae21dc279d01bc8
Nebulosa Cat <work@nebulosa-cat.com>
2024-01-27 23:25:47 +0800

    Fix and improve
    1. 刪除測試後忘記刪掉的寫死授權日期參數
    2. 感謝 @hirakujira 刪除無用的日期處理部分(要計算日期且無須格式化顯示因此原版的日期格式字串化不再需要)並且簡化計算邏輯

--- Panel/Surge-Pro/licensesCheck.js
+++ Panel/Surge-Pro/licensesCheck.js
@@ -1,27 +1,23 @@
-//日期處理
-const dateObject = new Date()
-const date = dateObject.getDate()
-const month = dateObject.getMonth()
-const year = dateObject.getFullYear()
-const monthNums = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
-
-let nowDate = `${year}-${monthNums[month]}-${date}`
-let licensesDate = "2024-05-28"
-
-//字串轉 Unix Time
-let nowDateStamp = new Date(nowDate);
-let licensesDateStamp = new Date(licensesDate);
-//相差日期計算
-let timeDiffCalc = function(nowDateStamp, licensesDateStamp){
-    result = parseInt(Math.abs(licensesDateStamp - nowDateStamp) / 86400000);
-    return result;
-}
-let timeDiff = timeDiffCalc(nowDateStamp,licensesDateStamp);
-
 if (typeof $argument != "undefined") {
-    let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+    const arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+
+    const licensesDate = arg.licensesDate;
+
+    const today = new Date();
+    today.setUTCHours(0, 0, 0, 0);
+
+    const todayTimestamp = Date.parse(today);
+    const licensesDateTimestamp = Date.parse(licensesDate);
+
+    // 相差日期計算
+    const timeDiffCalc = function (a, b) {
+        result = parseInt(Math.abs(a - b) / 86400000);
+        return result;
+    }
+    const timeDiff = timeDiffCalc(todayTimestamp, licensesDateTimestamp);
+
     //授權未過期
-    if (nowDate < arg.licensesDate){
+    if (todayTimestamp < licensesDateTimestamp) {
         $done({
             title: "Surge Pro® Licenses Status",
             content: `授權活躍中，再 ${timeDiff} 天後過期。`,
@@ -29,16 +25,15 @@ if (typeof $argument != "undefined") {
             'icon-color': '#5DAC81',
         });
     }
-    //授權已過期
-    else if (nowDate > arg.licensesDate){
+    //  //授權已過期
+    else if (todayTimestamp > licensesDateTimestamp) {
         $done({
             title: "Surge Pro® Licenses Status",
             content: `授權已過期 ${timeDiff} 天，請重新訂閱`,
             icon: 'xmark.icloud',
             'icon-color': '#DB4D6D',
         });
-    }
-    else{
+    } else {
         $done({
             title: "Surge Pro® Licenses Status",
             content: "今天即將過期，請重新訂閱",
